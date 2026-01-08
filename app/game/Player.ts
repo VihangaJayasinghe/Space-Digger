db4e9b4c840f9cb66efa729a4d00ac2364414c42
Vihanga Jayasinghe <jayasinghevihanga2@gmail.com>
2026-01-09 00:41:04 +0530

    set movement speed to 100

--- app/game/Player.ts
+++ app/game/Player.ts
@@ -11,7 +11,7 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
     D: Phaser.Input.Keyboard.Key;
   };
 
-  private baseMoveSpeed = 80; 
+  private baseMoveSpeed = 100; 
   private jetpackForce = -150; 
 
   // Particle Emitter
